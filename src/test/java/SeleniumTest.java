60e7beb4343ba26305c8bb38c3c405753588ac8e
Euleria <euleria@wp.pl>
2022-01-15 14:26:58 +0100

    add method getDriver(String browser)

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -2,16 +2,44 @@ import com.google.common.annotations.VisibleForTesting;
 
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
+import java.sql.Driver;
+
 public class SeleniumTest {
-    public static void main(String[] args) {
-        WebDriver driver = new FirefoxDriver();
-    }
 
     @Test
-    public void sampleTest () {
-        System.out.println("Hello!");
+    public void openGooglePage () {
+        WebDriver driver = getDriver("edge");
+        driver.get("https://www.google.pl/");
+
+    }
+
+    public WebDriver getDriver (String browser) {
+        String path;
+        WebDriver driver;
+        switch (browser) {
+            case "chrome":
+                //path = "C:\\Users\\euler\\Downloads\\chromedriver_win32\\chromedriver.exe";
+                //System.setProperty("webdriver.chrome.driver",path);
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                //path = "C:\\Users\\euler\\Downloads\\geckodriver-v0.30.0-win64\\geckodriver.exe";
+                //System.setProperty("webdriver.gecko.driver",path);
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                //path = "C:\\Users\\euler\\Downloads\\edgedriver_win64\\msedgedriver.exe";
+                //System.setProperty("webdriver.edge.driver",path);
+                driver = new EdgeDriver();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + browser);
+        }
+        return driver;
     }
 }
